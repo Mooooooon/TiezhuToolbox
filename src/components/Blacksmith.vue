d137a62d9f434b0236dbd4d62ed5c8a01e57d6f3
Moon <maoxianmoon@gmail.com>
2023-12-08 17:27:14 +0800

    读取属性

--- src/components/Blacksmith.vue
+++ src/components/Blacksmith.vue
@@ -21,6 +21,7 @@ import { exec } from 'child_process'
 import { useAdbStore } from '../store/adb'
 import { createWorker } from 'tesseract.js'
 import path from 'path'
+import sharp from 'sharp'
 
 const src = ref('screenshot.png?v=' + Math.random().toString(36).substring(7))
 const adbStore = useAdbStore()
@@ -30,7 +31,7 @@ const data = ref()
 const takeScreenshot = () => {
     const adbPath = path.join(process.cwd(), 'platform-tools', 'adb.exe')
     const adbCommand = adbPath + ' -s ' + selectedDeviceId + ' shell screencap -p /sdcard/screenshot.png && ' + adbPath + ' -s ' + selectedDeviceId + ' pull /sdcard/screenshot.png'
-    exec(adbCommand, (error, stdout, stderr) => {
+    exec(adbCommand, async (error, stdout, stderr) => {
         if (error) {
             console.error('截图错误:', error)
             return
@@ -40,23 +41,22 @@ const takeScreenshot = () => {
             return
         }
         console.log(stdout)
+        await primaryAttribute()
         src.value = 'screenshot.png?v=' + Math.random().toString(36).substring(7)
-        textOcr()
     })
     const activeElement = document.activeElement as HTMLElement
     if (activeElement) {
         activeElement.blur()
     }
 }
-const worker = await createWorker('chi_sim', 1, {
+const worker = await createWorker('eng+chi_sim', 1, {
     langPath: path.join(__dirname, 'lang-data'),
     logger: m => console.log(m)
 })
-const textOcr = () => {
-    (async () => {
-        data.value = await worker.recognize('screenshot.png')
-        console.log(data)
-    })()
+const textOcr = async (imagePath: string) => {
+    const { data: { text } } = await worker.recognize(imagePath)
+    const valuesArray = text.split(' ')
+    console.log(valuesArray)
 }
 
 // 定义一个销毁 worker 的函数
@@ -70,6 +70,18 @@ const destroyWorker = async () => {
 onUnmounted(() => {
     destroyWorker()
 })
+
+const primaryAttribute = async () => {
+    const processedImagePath = 'processed_screenshot.png'
+    const cropOptions = { left: 80, top: 270, width: 300, height: 40 }
+    await sharp('screenshot.png')
+        .resize(1600, 900)
+        .extract(cropOptions)
+        .toFile(processedImagePath)
+    textOcr('processed_screenshot.png')
+    return processedImagePath
+}
+
 </script>
 
 <style lang="scss"></style>
