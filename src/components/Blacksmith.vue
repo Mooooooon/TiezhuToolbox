fcb823d31259cfa91fa9ffda90b7c6ce8342b1a3
Moon <maoxianmoon@gmail.com>
2023-12-11 16:25:10 +0800

    显示副属性

--- src/components/Blacksmith.vue
+++ src/components/Blacksmith.vue
@@ -3,8 +3,16 @@
         <el-col :span="12">
             <el-button @click="takeScreenshot">截图</el-button>
             <el-descriptions class="gear-info" title="装备信息" :column="1">
-                <el-descriptions-item label="强化等级">+{{ enhancementLevel }}</el-descriptions-item>
-                <el-descriptions-item label="主属性">{{ primaryAttribute[0] }} {{ primaryAttribute[1] }}</el-descriptions-item>
+                <el-descriptions-item label="强化等级">
+                    {{ enhancementLevel !== undefined ? '+' + enhancementLevel : '' }}
+                </el-descriptions-item>
+                <el-descriptions-item label="主属性">
+                    {{ primaryAttribute[0] }} {{ primaryAttribute[1] }}
+                </el-descriptions-item>
+                <el-descriptions-item :label="attribute[0][0]">{{ attribute[0][1] }}</el-descriptions-item>
+                <el-descriptions-item :label="attribute[1][0]">{{ attribute[1][1] }}</el-descriptions-item>
+                <el-descriptions-item :label="attribute[2][0]">{{ attribute[2][1] }}</el-descriptions-item>
+                <el-descriptions-item :label="attribute[3][0]">{{ attribute[3][1] }}</el-descriptions-item>
             </el-descriptions>
         </el-col>
         <el-col :span="12">
@@ -32,6 +40,7 @@ const adbStore = useAdbStore()
 const selectedDeviceId = adbStore.device
 const enhancementLevel = ref()
 const primaryAttribute = ref(["", ""])
+const attribute = ref([["", ""]])
 
 const takeScreenshot = () => {
     const adbPath = path.join(process.cwd(), 'platform-tools', 'adb.exe')
@@ -48,8 +57,9 @@ const takeScreenshot = () => {
         console.log(stdout)
         await getPrimaryAttribute()
         await getEnhancementLevel()
+        await getAttribute()
         // src.value = 'screenshot.png?v=' + Math.random().toString(36).substring(7)
-        src.value = 'enhancement_level.png?v=' + Math.random().toString(36).substring(7)
+        src.value = 'attribute.png?v=' + Math.random().toString(36).substring(7)
     })
     const activeElement = document.activeElement as HTMLElement
     if (activeElement) {
@@ -105,12 +115,38 @@ const getPrimaryAttribute = async () => {
     const stringWithoutSpaces = text.replace(/\s+/g, '')
     primaryAttribute.value = stringWithoutSpaces.split(/(\d+%?)/).filter(Boolean)
 }
+
+const getAttribute = async () => {
+    const processedImagePath = 'attribute.png'
+    const cropOptions = { left: 45, top: 340, width: 410, height: 130 }
+    await sharp('screenshot.png')
+        .resize(1600, 900)
+        .extract(cropOptions)
+        .toFile(processedImagePath)
+    const text = await textOcr(processedImagePath)
+    const elements = text.split('\n').filter(function (element) {
+        return element.trim() !== ''
+    })
+    const processedElements = elements.map(function (element) {
+        const stringWithoutSpaces = element.replace(/\s+/g, '')
+
+        return stringWithoutSpaces.split(/(\d+%?)/).filter(Boolean)
+    })
+
+    console.log(processedElements)
+    attribute.value = processedElements
+}
 </script>
 
 <style lang="scss">
 .gear-info {
     margin-top: 20px;
     padding-right: 20px;
+}
+
+.gear-info .el-descriptions__label {
+    width: 100px;
+    display: inline-block;
 }
 </style>
   
