7f4a03b2a11302a3402ced2f6b4d5ce5dbb26944
Moon <maoxianmoon@gmail.com>
2024-01-03 20:25:09 +0800

    element load

--- components.d.ts
+++ components.d.ts
@@ -23,7 +23,6 @@ declare module 'vue' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
-    ElRadio: typeof import('element-plus/es')['ElRadio']
     ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRate: typeof import('element-plus/es')['ElRate']
